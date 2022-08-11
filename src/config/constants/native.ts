4d95f4ba67f42694b33c5d42ac5e65f0c75c1258
Verin1005 <daqingchong0809@gmail.com>
2022-08-11 11:13:02 +0800

    default token list

--- src/config/constants/native.ts
+++ src/config/constants/native.ts
@@ -2,14 +2,14 @@ import { ChainId } from "@/config/constants/chainId";
 import { Token } from "@/config/constants/types";
 type NativeMap = { [chainId: number]: Token };
 export const NATIVE: NativeMap = {
-    // [ChainId.BSC]: {
-    //     name: "BNB Token",
-    //     symbol: "BNB",
-    //     address: "",
-    //     chainId: ChainId.BSC,
-    //     decimals: 18,
-    //     logoURI: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/binance/info/logo.png",
-    // },
+    [ChainId.BSC]: {
+        name: "BNB Token",
+        symbol: "BNB",
+        address: "",
+        chainId: ChainId.BSC,
+        decimals: 18,
+        logoURI: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/binance/info/logo.png",
+    },
     [ChainId.RINKEBY]: {
         name: "ETH Token",
         symbol: "ETH",
@@ -18,14 +18,14 @@ export const NATIVE: NativeMap = {
         decimals: 18,
         logoURI: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/binance/info/logo.png",
     },
-    // [ChainId.Kovan]: {
-    //     name: "ETH Token",
-    //     symbol: "ETH",
-    //     address: "",
-    //     chainId: ChainId.Kovan,
-    //     decimals: 18,
-    //     logoURI: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/binance/info/logo.png",
-    // },
+    [ChainId.KOVAN]: {
+        name: "ETH Token",
+        symbol: "ETH",
+        address: "",
+        chainId: ChainId.KOVAN,
+        decimals: 18,
+        logoURI: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/binance/info/logo.png",
+    },
     // [ChainId.Mainnet]: {
     //     name: "ETH Token",
     //     symbol: "ETH",
@@ -34,14 +34,14 @@ export const NATIVE: NativeMap = {
     //     decimals: 18,
     //     logoURI: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/binance/info/logo.png",
     // },
-    // [ChainId.BSCTestnet]: {
-    //     name: "BNB Token",
-    //     symbol: "BNB",
-    //     address: "",
-    //     chainId: ChainId.BSCTestnet,
-    //     decimals: 18,
-    //     logoURI: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/binance/info/logo.png",
-    // },
+    [ChainId.BSC_TESTNET]: {
+        name: "BNB Token",
+        symbol: "BNB",
+        address: "",
+        chainId: ChainId.BSC_TESTNET,
+        decimals: 18,
+        logoURI: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/binance/info/logo.png",
+    },
 };
 
 export const getNative = (chainId: number) => {
--- src/config/tokens/index.ts
+++ src/config/tokens/index.ts
@@ -1,12 +1,20 @@
+import bsc from "./bsc.json";
+import bsctestnet from "./bsctestnet.json";
+import kovan from "./kovan.json";
 import rinkeby from "./rinkeby.json";
+import ropsten from "./ropsten.json";
 
-import { ChainId } from "../constants/chainId";
 import { Token } from "@/config/constants/types";
+import { ChainId } from "../constants/chainId";
 
 type TokenList = { [chainId: number]: Token[] };
 
 const TOKENLIST: TokenList = {
     [ChainId.RINKEBY]: rinkeby,
+    [ChainId.BSC]: bsc,
+    [ChainId.BSC_TESTNET]: bsctestnet,
+    [ChainId.KOVAN]: kovan,
+    [ChainId.ROPSTEN]: ropsten,
 };
 
 export default TOKENLIST;
